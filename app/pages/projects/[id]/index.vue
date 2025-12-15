15f1d22adb0058827de184e074a320083224e78d
Choquet Nicolas <nicolachoquet06250@gmail.com>
2025-12-15 18:18:48 +0100

    Mise en place d'un système de favoris.
    Un utilisateur possesseur d'un compte et connecté peut maintenant mettre et enlever des projets de ses favoris ainsi qu'accéder à une page "Mes favoris"

--- app/pages/projects/[id]/index.vue
+++ app/pages/projects/[id]/index.vue
@@ -124,8 +124,18 @@ async function toggleFavorite() {
           @click="toggleFavorite"
           :aria-pressed="isFavorite"
         >
-          <template v-if="isFavorite">★ Retirer des favoris</template>
-          <template v-else>☆ Ajouter aux favoris</template>
+          <template v-if="isFavorite">
+            <svg v-if="isFavorite" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="20" height="20">
+              <path d="M192 64C156.7 64 128 92.7 128 128L128 544C128 555.5 134.2 566.2 144.2 571.8C154.2 577.4 166.5 577.3 176.4 571.4L320 485.3L463.5 571.4C473.4 577.3 485.7 577.5 495.7 571.8C505.7 566.1 512 555.5 512 544L512 128C512 92.7 483.3 64 448 64L192 64z"/>
+            </svg>
+            Retirer des favoris
+          </template>
+          <template v-else>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="20" height="20">
+              <path d="M128 128C128 92.7 156.7 64 192 64L448 64C483.3 64 512 92.7 512 128L512 545.1C512 570.7 483.5 585.9 462.2 571.7L320 476.8L177.8 571.7C156.5 585.9 128 570.6 128 545.1L128 128zM192 112C183.2 112 176 119.2 176 128L176 515.2L293.4 437C309.5 426.3 330.5 426.3 346.6 437L464 515.2L464 128C464 119.2 456.8 112 448 112L192 112z"/>
+            </svg>
+            Ajouter aux favoris
+          </template>
         </button>
         <span v-if="favMsg" class="hint error">{{ favMsg }}</span>
       </div>
@@ -221,6 +231,7 @@ async function toggleFavorite() {
 }
 .actions { display: flex; gap: 8px; }
 .btn {
+  border: none;
   background-color: light-dark(#52c3ce, #181f29);
   color: light-dark(#000, #fff);
   padding: 10px;
@@ -229,10 +240,23 @@ async function toggleFavorite() {
   cursor: pointer;
   transition: background-color .2s ease-in-out,
   color .2s ease-in-out;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  > svg > path {
+    fill: light-dark(#000, #fff);
+    transition: fill .2s ease-in-out;
+  }
 
   &:hover {
     background-color: light-dark(#52c3ce66, #52c3ce);
     color: light-dark(#000, #000);
+
+    > svg > path {
+      fill: light-dark(#fff, #000);
+    }
   }
 }
 .btn.primary { background: #2b59c3; color: #fff; border-color: #2b59c3; }
